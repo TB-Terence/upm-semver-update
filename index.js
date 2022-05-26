e46e86a769f1882b094e537ca03e345d930ea7f1
Jiabin Tang <jiabintang@hotmail.com>
2022-05-26 19:32:24 -0400

    add version check

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('child_process');
+const { existsSync  } = require('fs');
 
 try {
-    getPackageJsonPath();
-    getAssemblyInfoPath();
+    const packageJson = getPackageJson();
+    const assemblyInfo = getAssemblyInfo();
+
+    console.log("Current package version: " + packageJson.version.toString());
+    const assemblyVersion = assemblyInfo.match(/AssemblyVersion\("\d.\d.\d(.\d)?"\)/);
+    const assemblyFileVersion = assemblyInfo.match(/AssemblyFileVersion\("\d.\d.\d.\d"\)/);
+    console.log(assemblyVersion);
+    console.log(assemblyFileVersion);
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    //const payload = JSON.stringify(github.context.payload, undefined, 2)
+    //console.log(`The event payload: ${payload}`);
 } catch (error) {
     core.setFailed(error.message);
 }
 
-function getPackageJsonPath()
+function getPackageJson()
 {
     const path = core.getInput('package-json-path');
+    if(!fs.execSync(path)) throw new Error('package.json not found at the given path: ' + path);
     console.log('package-json-path: ' + path);
     core.setOutput('package-json-path', path);
+    return require(path);
 }
 
-function getAssemblyInfoPath()
+function getAssemblyInfo()
 {
     const path = core.getInput('assembly-info-path');
+    if(!fs.execSync(path)) throw new Error('AssemblyInfo.cs not found at the given path: ' + path);
     console.log('assembly-info-path: ' + path);
     core.setOutput('assembly-info-path', path);
+    return fs.readFileSync(path);
 }
 
