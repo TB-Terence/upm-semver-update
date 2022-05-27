619c62e7abf09e6c774f5bc302738b0364efc51b
Jiabin Tang <jiabintang@hotmail.com>
2022-05-26 23:14:23 -0400

    add if check

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ try {
         core.setOutput('require-update', false);
         process.exit(0);
     }
+    console.log('require from index.js');
     core.setOutput('require-update', true);
     switch(type){
         case "major":
